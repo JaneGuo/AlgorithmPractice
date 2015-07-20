cce249c7425615949fbe2d7130bef10c56ea2378
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-19 18:05:45 -0700

    easy implement of stack using int array

--- C++ Examples/array_stack/array_stack/main.cpp
+++ C++ Examples/array_stack/array_stack/main.cpp
@@ -6,13 +6,60 @@
 //  Copyright (c) 2015 Jiancong Guo. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
+#define MAX_SIZE 101
+int A[MAX_SIZE];
+int top = -1;
+
+void Push(int x){
+    //overflow
+    if(top == MAX_SIZE -1){
+        printf("Error: stack overflow. \n");
+        return;
+    }
+    A[++top]=x;
+}
+
+void Pop(){
+    if(top == -1){
+        printf("Error: no element to pop. \n");
+        return;
+    }
+    top--;
+}
+
+int Top(){
+    if(top == -1){
+        printf("Error: stack is empty. \n");
+        return -1;
+    }
+    return A[top];
+}
+
+bool IsEmpty(){
+    if(top == -1) return true;
+    return false;
+}
+
+void Print(){
+    for(int i = 0; i<= top; i++){
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Push(2); Print();
+    Push(5);Print();
+	Push(10);Print();
+	Pop();Print();
+	Push(12);Print();
+    printf("%d\n",Top());
+    printf("%s\n", IsEmpty()?"true":"false");
+    
     return 0;
 }
 
