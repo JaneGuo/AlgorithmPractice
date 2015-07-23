547857ce8a60d163c698207a9fc02069cde6d116
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-23 14:00:44 -0700

    Initial Commit

---
+++ C++ Examples/Easy_Sum number Temple/Easy_Sum number Temple/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Easy_Sum number Temple
+//
+//  Created by Jiancong Guo on 7/23/15.
+//  Copyright (c) 2015 Jiancong Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
