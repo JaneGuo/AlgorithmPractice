91ffa46e705dca02c73e50c47bb32b59fc5a5615
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-23 16:29:03 -0700

    add explaination

--- C++ Examples/Easy_Sum number Temple/Easy_Sum number Temple/main.cpp
+++ C++ Examples/Easy_Sum number Temple/Easy_Sum number Temple/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 
 template <class T>
+//sum function could have 2 parameters or 3 parameters
 T sum(const T c_array[], int size, T sum =0){
     for(int i = 0; i<size; i++){
         sum += c_array[i];
@@ -20,7 +21,7 @@ int main(int argc, const char * argv[])
 {
 
     int a[] = {1,2,3,4,5};
-    std::cout<< "sum of array elements is "<<sum(a,5)<<std::endl;
+    std::cout<< "sum of array elements is "<<sum(a,5,1000)<<std::endl;
     
     float b[] = {1.1, 2.1, 3.1, 4.1, 5.1};
     std::cout<< "sum of array elements is "<< sum(b, 5)<<std::endl;
