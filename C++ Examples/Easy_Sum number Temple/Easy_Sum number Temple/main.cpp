2d68e0020031eec1d5a1bc2a5b3208b6ce13ade7
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-23 14:13:50 -0700

    an easy example to use template <class T>

--- C++ Examples/Easy_Sum number Temple/Easy_Sum number Temple/main.cpp
+++ C++ Examples/Easy_Sum number Temple/Easy_Sum number Temple/main.cpp
@@ -8,11 +8,23 @@
 
 #include <iostream>
 
+template <class T>
+T sum(const T c_array[], int size, T sum =0){
+    for(int i = 0; i<size; i++){
+        sum += c_array[i];
+    }
+    return sum;
+    
+}
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[] = {1,2,3,4,5};
+    std::cout<< "sum of array elements is "<<sum(a,5)<<std::endl;
+    
+    float b[] = {1.1, 2.1, 3.1, 4.1, 5.1};
+    std::cout<< "sum of array elements is "<< sum(b, 5)<<std::endl;
+    
     return 0;
 }
 
