1f0e8f2da5368ec5d4159c7d0865a12f9d598342
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-24 14:31:51 -0700

    Initial Commit

---
+++ C++ Examples/binary_search/binary_search/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  binary_search
+//
+//  Created by Jiancong Guo on 7/24/15.
+//  Copyright (c) 2015 Jiancong Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
