a5b88af1ede2987863ddcd90f7fdef36c500692a
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-23 18:13:14 -0700

    Initial Commit

--- C++ Examples/easy_swap_template/easy_swap_template/main.cpp
+++ C++ Examples/easy_swap_template/easy_swap_template/main.cpp
@@ -8,11 +8,30 @@
 
 #include <iostream>
 
+template <class T>
+
+void swap(T &a, T &b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a = 5;
+    int b = 6;
+    
+    swap(a, b);
+    std::cout<<"a is "<<a << " b is " << b <<std::endl;
+    
+    float m = 5.5;
+    float n = 6.5;
+    
+    swap(m, n);
+    std::cout<<"m is "<<m << " n is " << n <<std::endl;
+    
+    
     return 0;
 }
 
