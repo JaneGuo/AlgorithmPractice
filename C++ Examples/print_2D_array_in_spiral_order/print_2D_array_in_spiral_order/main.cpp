6b1354e4247eb4eca10c3cff56d530888ddbca8f
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-24 12:46:55 -0700

    Initial Commit

---
+++ C++ Examples/print_2D_array_in_spiral_order/print_2D_array_in_spiral_order/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  print_2D_array_in_spiral_order
+//
+//  Created by Jiancong Guo on 7/24/15.
+//  Copyright (c) 2015 Jiancong Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
