003fb980f2d2f9f396c7ab7e23fcb5d3b4878a5c
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-24 00:13:29 -0700

    permutation of char array

--- C++ Examples/permutaion of a char/permutaion of a char/main.cpp
+++ C++ Examples/permutaion of a char/permutaion of a char/main.cpp
@@ -8,11 +8,41 @@
 
 #include <iostream>
 
+void swap(char &x, char &y){
+    char temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+/* 
+ function to print permutaions of a char array
+ This function takes 2 parameters
+ 1. the char array
+ 2. size of the char array
+ */
+void permute(char a[], int size){
+    if(size <= 0){
+        std::cout<<"no permutation. \n";
+        return;
+    }
+    //print the original char array
+    std::cout<<"permutation: "<<a<<std::endl;
+    for(int i = 0; i < size; i++)
+        for(int j = i+1; j < size; j++){
+            swap(a[i],a[j]);
+            std::cout<<"permutation: "<<a<<std::endl;
+            swap(a[j],a[i]); //swap back
+        }
+}
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char a[] = "ABCD";
+    
+    permute(a, 4);
+    
     return 0;
 }
 
