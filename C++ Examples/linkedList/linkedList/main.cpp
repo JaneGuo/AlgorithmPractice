81d0c540b4bf024bac6edae90f80eb0bf58cedca
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-07-28 22:29:51 -0700

    Initial Commit

---
+++ C++ Examples/linkedList/linkedList/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  linkedList
+//
+//  Created by Jiancong Guo on 7/28/15.
+//  Copyright (c) 2015 Jiancong Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
