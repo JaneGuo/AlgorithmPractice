19b579264736f2af6603218516a9f69e00bfe963
guo3749 <Jane@Jiancongs-MacBook-Air.local>
2015-08-10 21:52:53 -0700

    Initial Commit

---
+++ C++ Examples/factorsOfNumber/factorsOfNumber/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  factorsOfNumber
+//
+//  Created by Jiancong Guo on 8/10/15.
+//  Copyright (c) 2015 Jiancong Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
